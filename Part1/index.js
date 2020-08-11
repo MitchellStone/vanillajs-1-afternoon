9692d6f5c677c5f329dfc3df2cdd04424cce5973
mitchell stone <themitch.stone@gmail.com>
2020-08-11 14:41:07 -0600

    perfected js code

--- Part1/index.js
+++ Part1/index.js
@@ -2,7 +2,7 @@ console.log("Tic Tac Toe")
 
 function play(){
     playerSpan = document.getElementById("player")
-    if(playerSpa.innerText === "X") {
+    if(playerSpan.innerText === "X") {
 playerSpan.innerText = 'O';
     } else{
         playerSpan.innerText = 'X';
