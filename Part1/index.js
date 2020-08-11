dedcdbfe01c9fafbafbd538108031f1cb7f67bff
mitchell stone <themitch.stone@gmail.com>
2020-08-11 14:40:03 -0600

    added player changer

--- Part1/index.js
+++ Part1/index.js
@@ -1,5 +1,11 @@
 console.log("Tic Tac Toe")
 
 function play(){
+    playerSpan = document.getElementById("player")
+    if(playerSpa.innerText === "X") {
+playerSpan.innerText = 'O';
+    } else{
+        playerSpan.innerText = 'X';
+    }
     
 }
