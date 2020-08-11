a0664625e7a9d1ba895ecb9eccd52bbe37a2935a
mitchell stone <themitch.stone@gmail.com>
2020-08-11 15:17:16 -0600

    added a clicked element

--- Part1/index.js
+++ Part1/index.js
@@ -1,15 +1,17 @@
 console.log("Tic Tac Toe")
 
-let morePlay = ('')
+let morePlay = [];
 function play(TicId){
  let  playerSpan = document.getElementById("player");
  let clickedElement = document.getElementById(TicId);
     if(playerSpan.innerText === "X") {
 playerSpan.innerText = 'O';
 clickedElement.innerText = 'X';
-    } else{
+morePlay[TicId] = 'X'
+    } else {
         playerSpan.innerText = 'X';
         clickedElement.innerText = 'O';
+        morePlay[TicId] = 'O'
     }
     
 }
